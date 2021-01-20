ce8fd13dc8488610472d062dab96bef2eafbc016
miladmozafari <milad.mozafari69@outlook.com>
2021-01-20 13:36:27 +0100

    KC normalization added.

--- predify/networks/presnet.py
+++ predify/networks/presnet.py
@@ -11,7 +11,7 @@ import torch.nn as nn
 import os
 import sys
 import numpy as np
-from ..modules import PCoder
+from ..modules import PCoder, PCoderN
 from ..utils import to_pair
 from . import PNetSameHP, PNetSeparateHP
 
@@ -173,6 +173,124 @@ class PResNet18V3SeparateHP(PNetSeparateHP):
         in_ch, out_ch, r, d, p = get_deep_info(resnet_seq, 46, 35)
         pmodule = Sequential(Upsample(scale_factor=2, mode='bilinear'), ConvTranspose2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1))
         self.pcoder5 = PCoder(pmodule, False, self.random_init)
+
+        def fw_hook1(m, m_in, m_out):
+            e = self.pcoder1(ff=m_out, fb=self.pcoder2.prd, target=self.input_mem, build_graph=self.build_graph, ffm=self.ffm1, fbm=self.fbm1, erm=self.erm1)
+            return e[0]
+        self.backbone.conv1.register_forward_hook(fw_hook1)
+
+        def fw_hook2(m, m_in, m_out):
+            e = self.pcoder2(ff=m_out, fb=self.pcoder3.prd, target=self.pcoder1.rep, build_graph=self.build_graph, ffm=self.ffm2, fbm=self.fbm2, erm=self.erm2)
+            return e[0]
+        self.backbone.layer1[1].conv2.register_forward_hook(fw_hook2)
+
+        def fw_hook3(m, m_in, m_out):
+            e = self.pcoder3(ff=m_out, fb=self.pcoder4.prd, target=self.pcoder2.rep, build_graph=self.build_graph, ffm=self.ffm3, fbm=self.fbm3, erm=self.erm3)
+            return e[0]
+        self.backbone.layer2[1].conv2.register_forward_hook(fw_hook3)
+
+        def fw_hook4(m, m_in, m_out):
+            e = self.pcoder4(ff=m_out, fb=self.pcoder5.prd, target=self.pcoder3.rep, build_graph=self.build_graph, ffm=self.ffm4, fbm=self.fbm4, erm=self.erm4)
+            return e[0]
+        self.backbone.layer3[1].conv2.register_forward_hook(fw_hook4)
+
+        def fw_hook5(m, m_in, m_out):
+            e = self.pcoder5(ff=m_out, fb=None, target=self.pcoder4.rep, build_graph=self.build_graph, ffm=self.ffm5, fbm=self.fbm5, erm=self.erm5)
+            return e[0]
+        self.backbone.layer4[1].conv2.register_forward_hook(fw_hook5)
+
+class PResNet18V3NSameHP(PNetSameHP):
+    """
+    3x3 conv transpose kernels and upsample with KC Normalization
+    """
+    def __init__(self, resnet, build_graph=False, random_init=True, ff_multiplier: float=0.33, fb_multiplier: float=0.33, er_multiplier: float=0.01):
+        super().__init__(resnet, 5, build_graph, random_init, ff_multiplier, fb_multiplier, er_multiplier)
+
+        resnet_seq = flatten_resnet(resnet)
+        
+        # create the first PCoder
+        in_ch, out_ch, r, d, p = get_deep_info(resnet_seq, 3, -1)
+        pmodule = Sequential(ConvTranspose2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1))
+        self.pcoder1 = PCoderN(pmodule, True, self.random_init)
+
+        # create the second PCoderN
+        in_ch, out_ch, r, d, p = get_deep_info(resnet_seq, 13, 3)
+        pmodule = Sequential(ConvTranspose2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1))
+        self.pcoder2 = PCoderN(pmodule, True, self.random_init)
+
+        # create the third PCoderN
+        in_ch, out_ch, r, d, p = get_deep_info(resnet_seq, 24, 13)
+        pmodule = Sequential(Upsample(scale_factor=2, mode='bilinear'), ConvTranspose2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1))
+        self.pcoder3 = PCoderN(pmodule, True, self.random_init)
+
+        # create the fourth PCoderN
+        in_ch, out_ch, r, d, p = get_deep_info(resnet_seq, 35, 24)
+        pmodule = Sequential(Upsample(scale_factor=2, mode='bilinear'), ConvTranspose2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1))
+        self.pcoder4 = PCoderN(pmodule, True, self.random_init)
+
+        # create the fifth PCoderN
+        in_ch, out_ch, r, d, p = get_deep_info(resnet_seq, 46, 35)
+        pmodule = Sequential(Upsample(scale_factor=2, mode='bilinear'), ConvTranspose2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1))
+        self.pcoder5 = PCoderN(pmodule, False, self.random_init)
+
+        def fw_hook1(m, m_in, m_out):
+            e = self.pcoder1(ff=m_out, fb=self.pcoder2.prd, target=self.input_mem, build_graph=self.build_graph, ffm=self.ffm, fbm=self.fbm, erm=self.erm)
+            return e[0]
+        self.backbone.conv1.register_forward_hook(fw_hook1)
+
+        def fw_hook2(m, m_in, m_out):
+            e = self.pcoder2(ff=m_out, fb=self.pcoder3.prd, target=self.pcoder1.rep, build_graph=self.build_graph, ffm=self.ffm, fbm=self.fbm, erm=self.erm)
+            return e[0]
+        self.backbone.layer1[1].conv2.register_forward_hook(fw_hook2)
+
+        def fw_hook3(m, m_in, m_out):
+            e = self.pcoder3(ff=m_out, fb=self.pcoder4.prd, target=self.pcoder2.rep, build_graph=self.build_graph, ffm=self.ffm, fbm=self.fbm, erm=self.erm)
+            return e[0]
+        self.backbone.layer2[1].conv2.register_forward_hook(fw_hook3)
+
+        def fw_hook4(m, m_in, m_out):
+            e = self.pcoder4(ff=m_out, fb=self.pcoder5.prd, target=self.pcoder3.rep, build_graph=self.build_graph, ffm=self.ffm, fbm=self.fbm, erm=self.erm)
+            return e[0]
+        self.backbone.layer3[1].conv2.register_forward_hook(fw_hook4)
+
+        def fw_hook5(m, m_in, m_out):
+            e = self.pcoder5(ff=m_out, fb=None, target=self.pcoder4.rep, build_graph=self.build_graph, ffm=self.ffm, fbm=self.fbm, erm=self.erm)
+            return e[0]
+        self.backbone.layer4[1].conv2.register_forward_hook(fw_hook5)
+
+class PResNet18V3NSeparateHP(PNetSeparateHP):
+    """
+    3x3 conv transpose kernels and upsample with KC Normalization
+    """
+    def __init__(self, resnet, build_graph=False, random_init=True, ff_multiplier: float=0.33, fb_multiplier: float=0.33, er_multiplier: float=0.01):
+        super().__init__(resnet, 5, build_graph, random_init, ff_multiplier, fb_multiplier, er_multiplier)
+
+        resnet_seq = flatten_resnet(resnet)
+        
+        # create the first PCoderN
+        in_ch, out_ch, r, d, p = get_deep_info(resnet_seq, 3, -1)
+        pmodule = Sequential(ConvTranspose2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1))
+        self.pcoder1 = PCoderN(pmodule, True, self.random_init)
+
+        # create the second PCoderN
+        in_ch, out_ch, r, d, p = get_deep_info(resnet_seq, 13, 3)
+        pmodule = Sequential(ConvTranspose2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1))
+        self.pcoder2 = PCoderN(pmodule, True, self.random_init)
+
+        # create the third PCoderN
+        in_ch, out_ch, r, d, p = get_deep_info(resnet_seq, 24, 13)
+        pmodule = Sequential(Upsample(scale_factor=2, mode='bilinear'), ConvTranspose2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1))
+        self.pcoder3 = PCoderN(pmodule, True, self.random_init)
+
+        # create the fourth PCoderN
+        in_ch, out_ch, r, d, p = get_deep_info(resnet_seq, 35, 24)
+        pmodule = Sequential(Upsample(scale_factor=2, mode='bilinear'), ConvTranspose2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1))
+        self.pcoder4 = PCoderN(pmodule, True, self.random_init)
+
+        # create the fifth PCoderN
+        in_ch, out_ch, r, d, p = get_deep_info(resnet_seq, 46, 35)
+        pmodule = Sequential(Upsample(scale_factor=2, mode='bilinear'), ConvTranspose2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1))
+        self.pcoder5 = PCoderN(pmodule, False, self.random_init)
 
         def fw_hook1(m, m_in, m_out):
             e = self.pcoder1(ff=m_out, fb=self.pcoder2.prd, target=self.input_mem, build_graph=self.build_graph, ffm=self.ffm1, fbm=self.fbm1, erm=self.erm1)
