3786c3b3c22681111a33b4283c49cbc47c0f29c1
Bhavin Choksi <44576110+bhavinc@users.noreply.github.com>
2021-06-01 10:16:47 +0200

    Add files via upload

---
+++ examples/PVGG/pvgg_instance.py
@@ -0,0 +1,63 @@
+from predify.modules import PCoder
+from predify.networks import PNetSameHP, PNetSeparateHP
+from torch.nn import Sequential, ConvTranspose2d, Upsample
+
+class PVGG16SeparateHP(PNetSeparateHP):
+    """
+    """
+    def __init__(self, vgg,build_graph=False, random_init=False, ff_multiplier: float=0.33, fb_multiplier: float=0.33, er_multiplier: float=0.01):
+        super().__init__(vgg, 5,build_graph, random_init, ff_multiplier, fb_multiplier, er_multiplier)
+
+        for m in self.backbone.features:
+            if isinstance(m, nn.ReLU):
+                m.inplace=False
+
+
+
+        # create the first PCoder
+        pmodule = Sequential(ConvTranspose2d(64, 3, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2)))
+        self.pcoder1 = PCoder(pmodule, True, self.random_init)
+
+        # create the second PCoder
+        pmodule = Sequential(ConvTranspose2d(128, 64, kernel_size=(10, 10), stride=(2, 2), padding=(4, 4)), ReLU(inplace=True))
+        self.pcoder2 = PCoder(pmodule, True, self.random_init)
+
+        # create the third PCoder
+        pmodule = Sequential(ConvTranspose2d(256, 128, kernel_size=(14, 14), stride=(2, 2), padding=(6, 6)), ReLU(inplace=True))
+        self.pcoder3 = PCoder(pmodule, True, self.random_init)
+
+        # create the fourth PCoder
+        pmodule = Sequential(ConvTranspose2d(512, 256, kernel_size=(14, 14), stride=(2, 2), padding=(6, 6)), ReLU(inplace=True))
+        self.pcoder4 = PCoder(pmodule, True, self.random_init)
+
+        # create the fourth PCoder
+        pmodule = Sequential(ConvTranspose2d(512, 512, kernel_size=(14, 14), stride=(2, 2), padding=(6, 6)), ReLU(inplace=True))
+        self.pcoder5 = PCoder(pmodule, False, self.random_init)
+
+
+
+        def fw_hook1(m, m_in, m_out):
+            e = self.pcoder1(ff=m_out, fb=self.pcoder2.prd, target=self.input_mem, build_graph=self.build_graph, ffm=self.ffm1, fbm=self.fbm1, erm=self.erm1)
+            return e[0]
+        self.backbone.features[3].register_forward_hook(fw_hook1)
+
+        def fw_hook2(m, m_in, m_out):
+            e = self.pcoder2(ff=m_out, fb=self.pcoder3.prd, target=self.pcoder1.rep, build_graph=self.build_graph, ffm=self.ffm2, fbm=self.fbm2, erm=self.erm2)
+            return e[0]
+        self.backbone.features[8].register_forward_hook(fw_hook2)
+
+        def fw_hook3(m, m_in, m_out):
+            e = self.pcoder3(ff=m_out, fb=self.pcoder4.prd, target=self.pcoder2.rep, build_graph=self.build_graph, ffm=self.ffm3, fbm=self.fbm3, erm=self.erm3)
+            return e[0]
+        self.backbone.features[15].register_forward_hook(fw_hook3)
+
+        def fw_hook4(m, m_in, m_out):
+            e = self.pcoder4(ff=m_out, fb=self.pcoder5.prd, target=self.pcoder3.rep, build_graph=self.build_graph, ffm=self.ffm4, fbm=self.fbm4, erm=self.erm4)
+            return e[0]
+        self.backbone.features[22].register_forward_hook(fw_hook4)
+
+        def fw_hook5(m, m_in, m_out):
+            e = self.pcoder5(ff=m_out, fb=None, target=self.pcoder4.rep, build_graph=self.build_graph, ffm=self.ffm5, fbm=self.fbm5, erm=self.erm5)
+            return e[0]
+        self.backbone.features[29].register_forward_hook(fw_hook5)
+
---
+++ predify/__init__.py
@@ -0,0 +1,177 @@
+import torch
+import os
+import toml
+import warnings
+
+def imports_str(is_3d=False, p_imports=None, indent=""):
+    imports =  indent + "from predify.modules import PCoder" + os.linesep
+    imports += indent + "from predify.networks import PNetSameHP, PNetSeparateHP" + os.linesep
+    if p_imports is None:
+        if is_3d:
+            imports += indent + "from torch.nn import Sequential, ConvTranspose3d, Upsample" + os.linesep
+        else:
+            imports += indent + "from torch.nn import Sequential, ConvTranspose2d, Upsample" + os.linesep
+    else:
+        for p_imp in p_imports:
+            imports += indent + p_imp + os.linesep
+    return imports
+
+def pcoder_str(pcoder_idx, module_name, src_shape, target_shape, has_feedback, same_param, module=None, indent=""):
+    s = indent + f"# PCoder number {pcoder_idx}" + os.linesep
+    
+    is_3d = len(src_shape) == 5
+
+    if module is None:
+        upsample = ""
+        if is_3d:
+            if src_shape[-3:] != target_shape[-3:]:
+                scale_factor = (target_shape[-3]/src_shape[-3], target_shape[-2]/src_shape[-2], target_shape[-1]/src_shape[-1])
+                upsample = f"Upsample(scale_factor={scale_factor}, mode='bilinear'),"
+            convt = f"ConvTranspose3d({src_shape[-4]}, {target_shape[-4]}, kernel_size=3, stride=1, padding=1)"
+        else:
+            if src_shape[-2:] != target_shape[-2:]:
+                scale_factor = (target_shape[-2]/src_shape[-2], target_shape[-1]/src_shape[-1])
+                upsample = f"Upsample(scale_factor={scale_factor}, mode='bilinear'),"
+            convt = f"ConvTranspose2d({src_shape[-3]}, {target_shape[-3]}, kernel_size=3, stride=1, padding=1)"
+        pmodule = f"pmodule = Sequential({upsample}{convt})"
+    else:
+        pmodule = f"pmodule = {module}"
+    
+    s += indent + pmodule + os.linesep
+    s += indent + f"self.pcoder{pcoder_idx} = PCoder(pmodule, {has_feedback}, self.random_init)" + os.linesep
+
+    fb = f"self.pcoder{pcoder_idx+1}.prd" if has_feedback else "None"
+    target = f"self.pcoder{pcoder_idx-1}.rep" if pcoder_idx > 1 else "self.input_mem"
+    ffm = "self.ffm" if same_param else f"self.ffm{pcoder_idx}"
+    fbm = "self.fbm" if same_param else f"self.fbm{pcoder_idx}"
+    erm = "self.erm" if same_param else f"self.erm{pcoder_idx}"
+
+    s += indent + f"def fw_hook{pcoder_idx}(m, m_in, m_out):" + os.linesep
+    s += indent + f"    e = self.pcoder{pcoder_idx}(ff=m_out, fb={fb}, target={target}, build_graph=self.build_graph, ffm={ffm}, fbm={fbm}, erm={erm})" + os.linesep
+    s += indent + f"    return e[0]" + os.linesep
+    s += indent + f"self.backbone.{module_name}.register_forward_hook(fw_hook{pcoder_idx})" + os.linesep
+    return s
+
+def header_str(number_of_pcoders, class_name, same_param, hyperparameters, indent=""):
+    if same_param:
+        inherit = "SameHP(PNetSameHP)"
+        ff = f"{float(hyperparameters['feedforward'])}"
+        fb = f"{float(hyperparameters['feedback'])}"
+        er = f"{float(hyperparameters['pc'])}"
+    else:
+        inherit = "SeparateHP(PNetSeparateHP)"
+        ff, fb, er = "(", "(", "("
+        for hp in hyperparameters:
+            ff += f"{float(hp['feedforward'])},"
+            fb += f"{float(hp['feedback'])},"
+            er += f"{float(hp['pc'])},"
+        ff = ff[:-1] + ")"
+        fb = fb[:-1] + ")"
+        er = er[:-1] + ")"
+    s  =  indent + f"class {class_name}{inherit}:" + os.linesep
+    s +=  indent + f"    def __init__(self, backbone, build_graph=False, random_init=False, ff_multiplier={ff}, fb_multiplier={fb}, er_multiplier={er}):" + os.linesep
+    s +=  indent + f"        super().__init__(backbone, {number_of_pcoders}, build_graph, random_init, ff_multiplier, fb_multiplier, er_multiplier)" + os.linesep
+    return s
+
+def parse_toml(config_file):
+    config = toml.load(config_file)
+
+    name = config.get("name", "network")
+    if name == "Net":
+        raise Exception("Config File Error! 'name' cannot have the value 'Net'. Please select other names.")
+    if not name.isidentifier():
+        raise Exception("Config File Error! 'name' should be a valid Python identifier.")
+    
+    input_size = config.get("input_size", None)
+    if input_size is None:
+        raise Exception("Config File Error! 'input_size' is not specified.")
+    if not isinstance(input_size, list) or len(input_size) <= 2 or len(input_size) >= 5:
+        raise Exception("Config File Error! 'input_size' should be defined as a list of [channels, height, width] for 2D inputs or [channels, depth, height, width] for 3D inputs.")
+    if len(input_size) == 4:
+        warnings.warn("Input dimension is set to 3D since the input size is defined as list of length 4: [channels, depth, height, width]. If you want to use 2D inputs, please specify it as [channels, height, width]")
+    input_size = tuple(input_size)
+
+    pcoders = config.get("pcoders", None)
+    layers = []
+    pmodules = []
+    hyperparams = []
+
+    if pcoders is None:
+        raise Exception("Config File Error! 'pcoders' is not specified.")
+    if not isinstance(pcoders, list) or len(pcoders) == 0:
+        raise Exception("Config File Error! 'pcoders' should be defined as a non-empty list.")
+    for pc_idx, pc in enumerate(pcoders):
+        if 'module' not in pc:
+            raise Exception(f"Config File Error! 'module' is not specified for PCoder number {pc_idx + 1} (starting from 1).")
+        layer = pc['module']
+        layers.append(layer)
+
+        predictor = pc.get("predictor", None)
+        if predictor is None:
+            warnings.warn(f"'predictor' is not defined for PCoder number {pc_idx}. A default module will be inferred based on the other PCoders.")
+        pmodules.append(predictor)
+        
+        if "hyperparameters" not in pc:
+            warnings.warn(f"'hyperparameters' is not defined for PCoder number {pc_idx}. Default values of feedforward=0.3, feedback=0.3, pc=0.01 will be set.")
+        hps = pc.get("hyperparameters", {"feedforward":0.3, "feedback":0.3, "pc":0.01})
+        if not ("feedforward" in hps and "feedback" in hps and "pc" in hps):
+            raise Exception(f"Config File Error! 'hyperparameters' for PCoder number {pc_idx + 1} should be a disctionary that contains 'feedforward', 'feedback', 'pc' as the keys.")
+        hyperparams.append(hps)
+
+    p_imports = config.get("imports", None)
+    if p_imports is not None:
+        if not isinstance(p_imports, list):
+            raise Exception(f"Config File Error! 'imports' should be defined as a list of strings.")
+
+    return name, layers, pmodules, hyperparams, p_imports, input_size
+        
+def predify(net, config_file, output_address=None):
+    """
+    Generates a python script that defines the predified version of the given network.
+    
+    Args:
+        net: a PyTorch compatible network object.
+        config_file: address to the .toml file that contains predify-required info.
+        output_address: address of the output script file. If `None`, the name of the network will be used.
+    """
+    config = parse_toml(config_file)
+    predify_core(net, *config, output_address)
+
+def predify_core(net, name, layers, pmodules, hps, p_imports, input_size, output_address):
+    net.eval()
+
+    layer_sizes = {}
+    def get_size(m, m_in, m_out):
+        layer_sizes[m] = m_out.shape
+
+    modules = []
+    for layer in layers:
+        modules.append(eval(f"net.{layer}"))
+        modules[-1].register_forward_hook(get_size)
+
+    x = torch.ones(1, *input_size)
+    with torch.no_grad():
+        net(x)
+
+    is_3d = len(input_size) == 5
+
+    if output_address is None:
+        output_address = f"{name.lower()}.py"
+    with open(output_address, "w") as f:
+        f.write(imports_str(is_3d, p_imports=p_imports))
+        f.write(os.linesep)
+
+        for same_param in [False]:
+            f.write(header_str(len(layers), f"{name}", same_param, hyperparameters=hps, indent=""))
+            f.write(os.linesep)
+
+            indent = " " * 8
+
+            target_shape = input_size
+            for idx, (module_name, module) in enumerate(zip(layers, modules), 1):
+                src_shape = layer_sizes[module]
+                s = pcoder_str(idx, module_name, src_shape, target_shape, idx < len(layers), same_param, module=pmodules[idx-1], indent=indent)
+                f.write(s)
+                f.write(os.linesep)
+                target_shape = src_shape
+            f.write(os.linesep)
